f12c2d2a21ff57460ce6653918fc4a86f6c17591
Andi <a0302840@unet.univie.ac.at>
2014-01-21 13:58:01 +0100

    added JDoc Comments

--- workspaceJSP2/Grasshopper/src/swe2013/dao/BookingDAO.java
+++ workspaceJSP2/Grasshopper/src/swe2013/dao/BookingDAO.java
@@ -4,12 +4,38 @@ import java.util.ArrayList;
 
 import swe2013.location.Bookings;
 
+/**
+ * Booking DAO
+ * @author Anreiter Simon, Moser Victoria Dorothy, Kocman Andreas
+ * Das Interface für die Buchungsverwaltung
+ */
 public interface BookingDAO {
 	
+	/**
+	 * saves a new booking to the SQL DB
+	 * @param hotelID the ID of the hotel
+	 * @param roomID the ID of the specific room that is booked
+	 * @param customerID the ID of the customer booking the room
+	 * @param beginDate the date of arrival
+	 * @param endDate the date of departure
+	 */
 	public void saveBooking(long hotelID, long roomID, long customerID, java.util.Date beginDate, java.util.Date endDate);
 	
+	
+	/**
+	 * returns all bookings for a room
+	 * @param hotelID The ID of the hotel
+	 * @param roomID The ID of the room
+	 * @return a Bookings Object consisting of all bookings for a specific room
+	 */
 	public Bookings getBookingsForRoom(long hotelID, long roomID);
 	
+	/**
+	 * Checks if a specific user booked a certain hotel
+	 * @param userID the ID of the user
+	 * @param hotelID the ID of the hotel
+	 * @return true if bookings are available in the db for a specific user, false if not
+	 */
 	public boolean userBookedHotel(long userID, long hotelID);
 	
 	public  ArrayList<String[]> bookingsForUser(long userID);
